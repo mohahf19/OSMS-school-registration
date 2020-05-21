818d2df0198fbbf94c85b96bedb419a8e8fe93a4
Muhammad Arham Khan <muhammadarhamkhan@gmail.com>
2020-05-21 15:58:19 +0300

    added teacher, ta relations

---
+++ app/Tutors.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tutors extends Model
+{
+    //
+}
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -44,9 +44,42 @@ class UserSeeder extends Seeder
             'password' => bcrypt('testtest'),
         ]);
 
+        DB::table('users')->insert([
+            'name' => 'Daenerys',
+            'surname' => 'Targaryen',
+            'email' => 'student1@test.com',
+            'address' => 'Dorm 77, Bilkent University',
+            'phone' => '+90 552 4299700',
+            'birthday' => Carbon::parse('1999-01-01'),
+            'password' => bcrypt('testtest'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Jon',
+            'surname' => 'Snow',
+            'email' => 'ta1@test.com',
+            'address' => 'Dorm 77, Bilkent University',
+            'phone' => '+90 552 4299700',
+            'birthday' => Carbon::parse('1999-01-01'),
+            'password' => bcrypt('testtest'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Khal',
+            'surname' => 'Drogo',
+            'email' => 'instructor1@test.com',
+            'address' => 'Dorm 77, Bilkent University',
+            'phone' => '+90 552 4299700',
+            'birthday' => Carbon::parse('1999-01-01'),
+            'password' => bcrypt('testtest'),
+        ]);
+
         $user1 = User::where('email', 'student@test.com')->first();
         $user2 = User::where('email', 'ta@test.com')->first();
         $user3 = User::where('email', 'instructor@test.com')->first();
+        $user4 = User::where('email', 'student1@test.com')->first();
+        $user5 = User::where('email', 'ta1@test.com')->first();
+        $user6 = User::where('email', 'instructor1@test.com')->first();
         
         DB::table('students')->insert([
             'user_id' => $user1->id,
@@ -59,6 +92,19 @@ class UserSeeder extends Seeder
         ]);
         DB::table('instructors')->insert([
             'user_id' => $user3->id,
+            'title' => 'Assistant Professor'
+        ]);
+        DB::table('students')->insert([
+            'user_id' => $user4->id,
+            'grade_points' => 145.1,
+            'total_credits' => 40,
+        ]);
+        DB::table('tas')->insert([
+            'user_id' => $user5->id,
+            'title' => 'TA'
+        ]);
+        DB::table('instructors')->insert([
+            'user_id' => $user6->id,
             'title' => 'Assistant Professor'
         ]);
     }
